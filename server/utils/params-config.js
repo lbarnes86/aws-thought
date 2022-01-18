b97a4b2c232415aa5b81f63eaa14b4e9e7161d5c
L II <lii@Ls-MacBook-Pro.lan>
2022-01-18 17:42:39 -0600

    add image endpoint

---
+++ server/utils/params-config.js
@@ -0,0 +1,16 @@
+const { v4: uuidv4 } = require('uuid');
+
+const params = fileName => {
+    const myFile = fileName.originalname.split('.');
+    const fileType = myFile[myFile.length - 1];
+  
+    const imageParams = {
+      Bucket: 'user-images-f45c5a77-073d-43cb-9856-36a400648e39',
+      Key: `${uuidv4()}.${fileType}`,
+      Body: fileName.buffer
+    };
+  
+    return imageParams;
+  };
+
+  module.exports = params;
